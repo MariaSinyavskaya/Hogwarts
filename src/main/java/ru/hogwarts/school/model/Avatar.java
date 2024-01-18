2cdaf0e9f5f91fb72c79363975ba6926a361aee3
Мария <maria.sinyavskaya8@gmail.com>
2024-01-18 19:10:49 +0300

    ДЗ 3

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -22,6 +22,15 @@ public class Avatar {
     public Avatar() {
     }
 
+    public Avatar(Long id, String filePath, long fileSize, String mediaType, byte[] data, Student student) {
+        this.id = id;
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+        this.mediaType = mediaType;
+        this.data = data;
+        this.student = student;
+    }
+
     public Long getId() {
         return id;
     }
