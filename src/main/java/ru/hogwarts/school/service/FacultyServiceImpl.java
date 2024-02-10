1717b335e812a65470f9dd4bb3538c5d65ff6faf
Мария <maria.sinyavskaya8@gmail.com>
2024-02-10 17:34:31 +0300

    Логирование и конфигурирование

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -11,47 +13,61 @@ import java.util.Collection;
 public class FacultyServiceImpl implements FacultyService {
     private final FacultyRepository facultyRepository;
 
+    Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
+
     public FacultyServiceImpl(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
     }
 
     @Override
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method for create faculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty findFaculty(Long id) {
+        logger.info("Was invoked method for find faculty by id");
         return facultyRepository.findById(id).get();
     }
 
     @Override
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Was invoked method for edit faculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public void deleteFaculty(Long id) {
+        logger.info("Was invoked method for delete faculty by id");
+        logger.warn("Faculty with id = {} was deleted", id);
         facultyRepository.deleteById(id);
     }
 
     @Override
     public Collection<Faculty> findByColor(String color) {
+        logger.info("Was invoked method for find faculties by color");
+        logger.debug("Requesting a collection of faculties with {} color", color);
         return facultyRepository.findByColor(color);
     }
 
     @Override
     public Collection<Faculty> findByNameOrColor(String name, String color) {
+        logger.info("Was invoked method for find faculties by name or color");
+        logger.debug("Requesting a collection of faculties with name {} or {} color", name, color);
         return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(name, color);
     }
 
     @Override
     public Collection<Faculty> getAllFaculties() {
+        logger.info("Was invoked method for find all faculties");
         return facultyRepository.findAll();
     }
 
     @Override
     public Collection<Student> findStudentsOfFaculty(Long id) {
+        logger.info("Was invoked method for find students of faculty by id");
+        logger.debug("Requesting the students of faculty with id = {}", id);
         return facultyRepository.getReferenceById(id).getStudents();
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -13,62 +15,79 @@ import java.util.List;
 public class StudentServiceImpl implements StudentService{
     private final StudentRepository studentRepository;
 
+    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
+
     public StudentServiceImpl(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     @Override
     public Student createStudent(Student student) {
+        logger.info("Was invoked method for create student");
         return studentRepository.save(student);
     }
 
     @Override
     public Student findStudent(Long id) {
+        logger.info("Was invoked method for find student by id");
         return studentRepository.findById(id).get();
     }
 
     @Override
     public Student editStudent(Student student) {
+        logger.info("Was invoked method for edit student");
         return studentRepository.save(student);
     }
 
     @Override
     public void deleteStudent(Long id) {
+        logger.info("Was invoked method for delete student by id");
+        logger.warn("Student with id = {} was deleted", id);
         studentRepository.deleteById(id);
     }
 
     @Override
     public Collection<Student> findByAge(int age) {
+        logger.info("Was invoked method for find students by age");
+        logger.debug("Requesting a collection of students with age {}", age);
         return studentRepository.findByAge(age);
     }
 
     @Override
     public Collection<Student> findByAgeBetween(int minAge, int maxAge) {
+        logger.info("Was invoked method for find students by age between min and max values");
+        logger.debug("Requesting a collection of students with ages between {} and {}", minAge, maxAge);
         return studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
     @Override
     public Collection<Student> getAllStudents() {
+        logger.info("Was invoked method for find all students");
         return studentRepository.findAll();
     }
 
     @Override
     public Faculty findFacultyOfStudent(Long id) {
+        logger.info("Was invoked method for find faculty of student by id");
+        logger.debug("Requesting the faculty of student with id = {}", id);
         return studentRepository.getReferenceById(id).getFaculty();
     }
 
     @Override
     public int getCountOfStudents() {
+        logger.info("Was invoked method for get count of student");
         return studentRepository.getCountOfStudents();
     }
 
     @Override
     public double getAvgOfStudentsAge() {
+        logger.info("Was invoked method for get average of student's age");
         return studentRepository.getAvgOfStudentsAge();
     }
 
     @Override
     public List<Student> getFiveLastStudents() {
+        logger.info("Was invoked method for get five last students in db");
         return studentRepository.getFiveLastStudents();
     }
 }
